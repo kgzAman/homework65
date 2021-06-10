a67a8a24c64e5fc5a102e733e89304f757e1bedc
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-06-10 18:33:28 +0600

    V10061703__fix_ProductTable

--- src/main/resources/db/migration/V10061703__fix_ProductTable.sql
+++ src/main/resources/db/migration/V10061703__fix_ProductTable.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE products
---  ADD column image VARCHAR(128) not null after `price`,
---     add column types varchar (128) not null ;
---
+ALTER TABLE products
+ ADD column image VARCHAR(128) not null after `price`,
+    add column types varchar (128) not null ;
+
 
