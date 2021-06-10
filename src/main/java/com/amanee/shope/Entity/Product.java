6c2ba2d228957b69cfe1e1b75384d9562b0b9857
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-06-10 18:32:00 +0600

    class product

--- src/main/java/com/amanee/shope/Entity/Product.java
+++ src/main/java/com/amanee/shope/Entity/Product.java
@@ -12,25 +12,25 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "products")
 public class Product extends baseEntity {
-//
-//    @Column(name ="name",length = 128)
-//    private String name;
-//
-//    @Column(name ="description",length = 224)
-//    private String description;
-//
-//    @Column(name ="price")
-//    private BigDecimal price;
-//
-//    @Column(name ="amount")
-//    private int amount;
-//
-//    @Column(name = "image")
-//    private String img;
-//
-//
-//    @Enumerated(EnumType.STRING)
-//    @Column(name="types")
-//    private Type type;
+
+    @Column(name ="name",length = 128)
+    private String name;
+
+    @Column(name ="description",length = 224)
+    private String description;
+
+    @Column(name ="price")
+    private BigDecimal price;
+
+    @Column(name ="amount")
+    private int amount;
+
+    @Column(name = "image")
+    private String img;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="types")
+    private Type type;
 
 }
