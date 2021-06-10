af9c37ba5af5c8fee1d6afde4029e52aa39c0bbf
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-06-10 18:32:56 +0600

    V10061206 sql.file

--- src/main/resources/db/migration/V10061806__some.sql
+++ src/main/resources/db/migration/V10061806__some.sql
@@ -1,7 +1,7 @@
--- use `shoponline`;
--- insert into `clients`('address','email','name') values
--- ('Бейкер-стрит, 221b','Sherlockgmail.com','Sherlock'),
--- ('England, Hogwards','HarryPottergmail.com','Harry Potter');
---
--- insert into `products`('name','description','price','amount','image.png','types') values
--- ('phone','iphoneX',1500,5,'some','PHONE');
+use `shoponline`;
+insert into `clients`('address','email','name') values
+('Бейкер-стрит, 221b','Sherlockgmail.com','Sherlock'),
+('England, Hogwards','HarryPottergmail.com','Harry Potter');
+
+insert into `products`('name','description','price','amount','image.png','types') values
+('phone','iphoneX',1500,5,'some','PHONE');
