ff8b51d1556c2e52b787cf427b9d60ebffad6942
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-06-14 18:56:19 +0600

    getClient method

--- src/main/java/com/amanee/shope/Services/ClientService.java
+++ src/main/java/com/amanee/shope/Services/ClientService.java
@@ -5,15 +5,13 @@ import com.amanee.shope.Repositories.ClientRepository;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Data
 public class ClientService {
     private final ClientRepository clientRepository;
 
-//    public Client getById(Integer id){
-//        return clientRepository.findById(id).orElseThrow();
-//    }
+    public Client getById(Integer id){
+        return clientRepository.findById(id).orElseThrow();
+    }
 
 }
